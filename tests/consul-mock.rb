525d29439f8df89e3a107f83ed8aad9f117043c0
Dan Farrell <dfarrell@bloomhealthco.com>
2014-07-11 13:31:21 -0500

    after much fighting, managed to get tests passign agin

--- tests/consul-mock.rb
+++ tests/consul-mock.rb
@@ -2,7 +2,7 @@
 ## Consul Mock-Up
 
 require 'sinatra'
-require 'json' 
+require 'json'
 
 set :port, 8500
 
@@ -14,27 +14,26 @@ mock_data = JSON.parse '{"Node":{
         }
       }'
 
-get "/v1/catalog/node/consul-client" do
-    mock_data.to_json
+get '/v1/catalog/node/consul-client' do
+  mock_data.to_json
 end
 
 get '/v1/health/node/consul-client' do
-    '[{"Notes":"","ServiceName":"","Status":"passing","ServiceID":"","Output":"","CheckID":"serfHealth","Node":"consul-client","Name":"Serf Health Status"},{"Notes":"","ServiceName":"service1","Status":"passing","ServiceID":"service1","Output":"","CheckID":"service:service1","Node":"consul-client","Name":"Service \'service1\' check"},{"Notes":"","ServiceName":"service2","Status":"failing","ServiceID":"service2","Output":"","CheckID":"service:service2","Node":"consul-client","Name":"Service \'service2\' check"}]'
+  '[{"Notes":"","ServiceName":"","Status":"passing","ServiceID":"","Output":"","CheckID":"serfHealth","Node":"consul-client","Name":"Serf Health Status"},{"Notes":"","ServiceName":"service1","Status":"passing","ServiceID":"service1","Output":"","CheckID":"service:service1","Node":"consul-client","Name":"Service \'service1\' check"},{"Notes":"","ServiceName":"service2","Status":"failing","ServiceID":"service2","Output":"","CheckID":"service:service2","Node":"consul-client","Name":"Service \'service2\' check"}]'
 end
 
-get "/v1/catalog/service/service1" do
-    '[{"Node":"consul-client","Address":"10.1.2.3","ServiceID":"service1","ServiceName":"service1","ServiceTags":["web","s1"],"ServicePort":80}]'
+get '/v1/catalog/service/service1' do
+  '[{"Node":"consul-client","Address":"10.1.2.3","ServiceID":"service1","ServiceName":"service1","ServiceTags":["web","s1"],"ServicePort":80}]'
 end
 
-get "/v1/catalog/service/service2" do
-    '[{"Node":"consul-client","Address":"10.1.2.3","ServiceID":"service2","ServiceName":"service2","ServiceTags":["web","s2"],"ServicePort":81}]'
+get '/v1/catalog/service/service2' do
+  '[{"Node":"consul-client","Address":"10.1.2.3","ServiceID":"service2","ServiceName":"service2","ServiceTags":["web","s2"],"ServicePort":81}]'
 end
 
-get "/mock-ready" do 
-    'ready'
+get '/mock-ready' do
+  'ready'
 end
 
-get "/mock-end" do 
-    Kernel.exit!
+get '/mock-end' do
+  Kernel.exit!
 end
-
